998cacc988e47dcd6d921054def772e0b17c172d
txkem <txkemru@gmail.com>
2025-07-15 14:34:36 +0800

    Initial Commit

---
+++ resume-app/resume_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  resume_appApp.swift
+//  resume-app
+//
+//  Created by Владимир Пушков on 15.07.2025.
+//
+
+import SwiftUI
+
+@main
+struct resume_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ resume-appTests/resume_appTests.swift
@@ -0,0 +1,17 @@
+//
+//  resume_appTests.swift
+//  resume-appTests
+//
+//  Created by Владимир Пушков on 15.07.2025.
+//
+
+import Testing
+@testable import resume_app
+
+struct resume_appTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
